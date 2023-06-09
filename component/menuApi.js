7ad479fc7ed9a99170124ee69328009f3a45d64e
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2023-06-09 13:29:32 +0530

    first commit

---
+++ component/menuApi.js
@@ -0,0 +1,84 @@
+const Menu =[
+    {
+    id:1,
+    image: "images/maggie.jpg",
+    name: "maggie",
+    category: "breakfast",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+  },
+  {
+    id:2,
+    image: "images/chana-masala.jpg",
+    name: "chole",
+    category: "lunch",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+    
+  },
+  {
+    id:3,
+    image: "images/pizza.jpg",
+    name: "pizza",
+    category: "lunch",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+
+  },
+  {
+    id:4,
+    image: "images/sandwich.jpg",
+    name: "sandwitch",
+    category: "evening",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+  
+  },
+  {
+    id:5,
+    image: "images/burger.jpg",
+    name: "Burger",
+    category: "dinner",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+   
+  },
+  {
+    id:6,
+    image: "images/paratha.jpg",
+    name: "paratha",
+    category: "dinner",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+   
+  },
+  {
+    id:7,
+    image: "images/soup.jpg",
+    name: "soup",
+    category: "dinner",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+  
+  },
+  {
+    id:8,
+    image: "images/momos.jpg",
+    name: "momoos",
+    category: "breakfast",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+
+  },
+  {
+    id:9,
+    image: "images/kimchi.jpg",
+    name: "spigety",
+    category: "korean",
+    price: 25,
+    description: "i love maggie ..ohh really awosome...yummmy...Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa tempore vitae modi vero.Aliquid, tempora. Tempore, temporibus quo.",
+
+  },
+];
+
+export default Menu;
